3aded0668e374f64484244bb61cd9e05909b6223
Alice Lindkvist <alicelindkvist@MacBook-Air-som-tillhor-Alice.local>
2025-11-14 10:54:25 +0100

    Utvecklat sidan

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
   document.querySelectorAll(".bock").forEach(button => {
     button.addEventListener("click", function () {
       const input = this.previousElementSibling; 
-
    
       if (this.textContent === "✔️") {
         this.textContent = "";    
@@ -26,17 +25,40 @@
     
   });
 
+    document.querySelector(".rensaNotes").addEventListener("click", function () {
+    document.querySelector(".text").value = "";
+ });
+
+ document.addEventListener("DOMContentLoaded", function () {
+
+    document.querySelectorAll('.minus').forEach(function(minusBtn) {
+        minusBtn.addEventListener("click", function () {
+            const listrad = this.closest('.listrad');
+            listrad.remove();
+        });
+    });
+
+
   document.querySelector(".läggTill").addEventListener("click", function () {
     const listrad = document.createElement('div');
     listrad.classList.add('listrad');
 
-    const input = document.createElement('input');
-    input.classList.add('lista');
-
     const button = document.createElement('button');
     button.classList.add('bock');
 
-    listrad.append(input, button);
+    const input = document.createElement('input');
+    input.classList.add('lista');
+
+    const minusBtn = document.createElement('button');
+    minusBtn.innerText ="➖";
+    minusBtn.classList.add('minus');
+
+    listrad.append(button, input, minusBtn);
 
     document.getElementById('rader').appendChild(listrad);
-  })
+    
+    minusBtn.addEventListener("click", function () {
+      listrad.remove();
+   });
+  });
+ });
